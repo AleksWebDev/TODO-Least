152a8e2af74c9ec1c21c58fde3dda9f0a4be8d0b
AleksWebDev <lavrovweb@gmail.com>
2024-04-08 23:33:39 +0300

    todoleast is ready

--- js/main.js
+++ js/main.js
@@ -2,15 +2,19 @@
 const btn = document.querySelector('.form__btn');
 const input = document.querySelector('.input');
 const taskContainer = document.querySelector('.tasks__container');
+const hints = document.querySelector('.hint');
 
 let allData = JSON.parse(localStorage.getItem('task')) || [];
+
+
 
 btn.addEventListener('click', function(){
 
     let val = input.value;
 
     if(!val){
-        console.log('Writte down task')
+        hints.innerHTML = 'Заполните пустое поле!!!'
+        hints.classList.add('hint__err');
         return '';
     }
 
@@ -18,17 +22,19 @@ btn.addEventListener('click', function(){
         name: val,
         isCheked: false,
     })
-
     fillHTML();
     setLS();
 
+    hints.innerHTML = 'Что делаем, сколько времени тратим, какой результат получим';
+    hints.classList.remove('hint__err');
     input.value = '';
 })
 
+/* Rendering HTML templates in loading browser */
 window.addEventListener('load', function(){
     fillHTML();
+    complitedTasks();
 })
-
 const fillHTML = () => {
     taskContainer.innerHTML = '';
 
@@ -37,65 +43,69 @@ const fillHTML = () => {
     })
 }
 
-const setLS = () => {
-    localStorage.setItem('task', JSON.stringify(allData));
-}
-
 const createTemplates = (item, index) => {
     return `
-    <div class="task">
+    <div class="task" ${item.isCheked ? 'checked' : ''}>
         <div class="text">${item.name}</div>
         <div class="task__control">
-            <input type="checkbox" class="checkbox">
+            <input onclick="complitedTasks(${index})"  ${item.isCheked ? 'checked' : ''} type="checkbox" class="checkbox">
             <button onclick="deliteTask(${index})" class="delite__task">&#10060</button>
         </div>
     </div>
     `
 }
+/* ----- */
 
+
+/* Function for adding items to localStorage */
+const setLS = () => {
+    localStorage.setItem('task', JSON.stringify(allData));
+}
+
+const complitedTasks = (idx) => {
+
+    allData[idx].isCheked = !allData[idx].isCheked;
+    let arrComplitedTask = [];
+    let arrActiveTask = [];
+
+    allData.forEach((item, index) => {
+        if(item.isCheked){
+            arrComplitedTask.push(item);
+        }else{
+            arrActiveTask.push(item);
+        }
+    })
+
+    allData = [...arrActiveTask, ...arrComplitedTask];
+
+    fillHTML();
+    setLS();
+}
+
+/* Function delite task */
 const deliteTask = (index) => {
     allData.splice(index, 1);
     setLS();
     fillHTML();
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /* active placeholder */
-
-/* const fakePLaceholder = input.closest('.fake__placeholder');
+const formTitle = document.querySelector('.form__title');
+const hint = document.querySelector('.hint');
 
 input.addEventListener('focus', function(){
-    input.nextElementSibling.classList.add('active');
+        formTitle.classList.add('active');
 })
 
 input.addEventListener('blur', function(){
-    if(input.value.length > 0){
-        input.nextElementSibling.classList.add('active');
-    }
-    
-    input.nextElementSibling.classList.remove('active');
-    
-}) */
+        if(input.value.length > 0){
+            formTitle.classList.add('active');
+        }
 
->>>>>>> html
+        formTitle.classList.remove('active');
+})
+
+
+
+
+
